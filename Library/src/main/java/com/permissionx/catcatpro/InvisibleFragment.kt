1eed5fbcb8c0ebfdb0d30d058e7638ea774d83f0
catcatproer <1901749687@qq.com>
2023-06-05 18:17:08 +0800

    完成PermissionsX库的开发与测试工作

---
+++ Library/src/main/java/com/permissionx/catcatpro/InvisibleFragment.kt
@@ -0,0 +1,33 @@
+package com.permissionx.catcatpro
+
+import android.content.pm.PackageManager
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+
+@Suppress("DEPRECATION")
+typealias PermissionCallback = (Boolean, List<String>) -> Unit
+class InvisibleFragment: Fragment(){
+//    private var callback:((Boolean, List<String>) -> Unit) ? = null
+    private var callback: PermissionCallback?=null
+    fun requestNow(cb: PermissionCallback, vararg permissions:String){
+        callback = cb
+        requestPermissions(permissions, 1)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1){
+            val deniedList = ArrayList<String>()
+            for ((index, result) in grantResults.withIndex()){
+                if (result != PackageManager.PERMISSION_GRANTED){
+                    deniedList.add(permissions[index])
+                }
+            }
+            val allGranted =  deniedList.isEmpty()
+            callback?.let { it(allGranted, deniedList) }
+        }
+    }
+}
--- app/src/main/java/com/permissionx/app/MainActivity.kt
+++ app/src/main/java/com/permissionx/app/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.permissionx.app
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,24 +15,41 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
+import com.permissionx.app.databinding.ActivityMainBinding
 import com.permissionx.app.ui.theme.PermissionXTheme
+import com.permissionx.catcatpro.PermissionX
+import java.net.URL
 
-class MainActivity : ComponentActivity() {
+class MainActivity :AppCompatActivity() {
+    lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            PermissionXTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+        viewBinding.makeCallBtn.setOnClickListener{
+            PermissionX.request(this, Manifest.permission.CALL_PHONE){ allGrandted, deniedList ->
+                if(allGrandted){
+                    call()
+                }else{
+                    Toast.makeText(this, "You denied $deniedList", Toast.LENGTH_SHORT).show()
                 }
             }
         }
     }
+
+    private fun call(){
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10010")
+          startActivity(intent)
+        }catch (e: SecurityException){
+            e.printStackTrace()
+        }
+    }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
